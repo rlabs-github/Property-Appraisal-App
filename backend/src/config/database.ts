71e34af9c45e3e1eb07c11c2f89e0433e120db21
rlabs-github <rlabs.github.com>
2025-04-18 13:33:25 -0500

    Update: Add a Log Before Connecting

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -35,6 +35,13 @@ export const db = {
     }
   },
   connectDatabase: async () => {
+    console.log('DB ENV Config:', {
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      database: process.env.POSTGRES_DB,
+    });    
     await pool.connect();
     console.log('Database connected successfully');
   },
